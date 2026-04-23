366f4fadbe60d526f15d7ceda9652f71a31116f4
Shafiq Mohammed <shafiqsnss@gmail.com>
2026-04-23 18:52:49 -0400

    Address warnings

--- src/main/java/basics/GrossPayCalculator.java
+++ src/main/java/basics/GrossPayCalculator.java
@@ -3,7 +3,7 @@ package basics;
 import java.util.Scanner;
 
 public class GrossPayCalculator {
-    public static void main(String[] args) {
+    static void main () {
         System.out.println("How many hours did you work?");
         Scanner scanner = new Scanner(System.in);
         int hours = scanner.nextInt();
